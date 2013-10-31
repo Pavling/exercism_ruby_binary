be65171c58f31bd7b8cd921639821e0f110d9da0
Michael Pavling <pavling@gmail.com>
2013-10-31 13:11:29 +0000

    refactoring

--- binary.rb
+++ binary.rb
@@ -1,14 +1,12 @@
 class Binary
     def initialize(binary)
-      @binary = binary unless binary.to_s.match(/[^10]/)
+      @binary = binary.to_s.match(/[^10]/) ? 0 : binary
     end
 
     def to_decimal
-      decimal = 0
-      @binary.to_s.reverse.chars.each_with_index do |char, index|
-        decimal += 2**index if char == '1'
-      end
-      decimal
+      @binary.to_s.reverse.chars.each_with_index.map do |char, index|
+        char == '1' ? 2**index : 0
+      end.reduce(:+)
     end
 
 end
